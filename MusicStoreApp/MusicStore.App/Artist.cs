2dc31ad1aeef3ac4883361a05519d35f8eb35a1b
Carlos Osoria <carlos.osoria@gmail.com>
2021-10-15 21:28:19 -0500

    Added the artist

---
+++ MusicStoreApp/MusicStore.App/Artist.cs
@@ -0,0 +1,18 @@
+﻿
+namespace MusicStore.App
+{
+    public class Artist
+    {
+        private string _firstName;
+        private string _lastName;
+
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public Artist(string firstName, string lastName)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+        }
+    }
+}

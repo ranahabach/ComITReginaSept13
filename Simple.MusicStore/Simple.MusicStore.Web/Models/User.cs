c2f3b4c105397f4b33568002aca2ddb947cd00ea
Carlos Osoria <carlos.osoria@gmail.com>
2021-10-20 22:11:20 -0500

    Added the simple music store app

---
+++ Simple.MusicStore/Simple.MusicStore.Web/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicStore2.Web.Models
+{
+    public class User
+    {
+        public int Id { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public string EmailAddress { get; }
+    }
+}
